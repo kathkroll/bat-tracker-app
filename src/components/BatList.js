ab067e708917d347125eb15f7af5e032258e9046
Katherine Kroll <kathkroll1@gmail.com>
2019-09-14 16:27:05 -0500

    Add MaterialUI and make the bat list a table

--- src/components/BatList.js
+++ src/components/BatList.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 export default class BatList extends Component {
   
   render() {
     const { bats } = this.props;
 
-    const batList = bats.map(bat =>
-      <li key={bat.id}>{bat.name}</li>
-    );
+    const batList = bats.map(function(bat) {
+      return (
+        <TableRow key={bat.id}>
+          <TableCell>{bat.name}</TableCell>
+          <TableCell>{bat.currentWeight}</TableCell>
+          <TableCell>{bat.currentLocation}</TableCell>
+          <TableCell></TableCell>
+        </TableRow>
+      );
+    });
 
     return (
-      <div>
-        <ul>
-          {batList}
-        </ul>
-      </div>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Weight</TableCell>
+              <TableCell>Location</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {batList}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
