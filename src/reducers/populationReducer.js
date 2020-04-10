2710d63ef4cc60bc492a5b918097e1abb564de38
Katherine Kroll <katherine@thisisalice.com>
2020-04-10 10:41:27 -0500

    Update populationReducer.js

--- src/reducers/populationReducer.js
+++ src/reducers/populationReducer.js
@@ -3,6 +3,7 @@ import data from '../mockData';
 export default (state = data.populationReducer, action) => {
   switch (action.type) {
     default:
+      console.log(state);
       return state
   }
 }
